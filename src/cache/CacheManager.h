2d5bc5a1a56005974a90d857a6779ec6bc3a88bd
xiaguanggun <xiaguanggun@gmail.com>
2024-05-07 15:47:41 +0800

    M cache

--- src/cache/CacheManager.h
+++ src/cache/CacheManager.h
@@ -9,16 +9,20 @@ using std::vector;
 class CacheManager {
 public: 
     static CacheManager * getInstance();
-    static void init(const string& filename, size_t threadNum);
-    static LRUCache& getCache(size_t idx);
+    static void init(size_t threadNum);
+    static LRUCache& getKeyWordCache(size_t idx);
+    static LRUCache& getWebQueryCache(size_t idx);
     static void updateCache();
+    static void writeToFile();
 private:
     CacheManager();
     ~CacheManager(){}
     static void destory();
 private: 
     static CacheManager * _pInstance;
-    vector<LRUCache> _caches;
+    size_t _threadNum;
+    vector<LRUCache> _cachesKeyWord; // 关键字缓存
+    vector<LRUCache> _cachesWebQuery; // 网页缓存
 };
 
 // 构造析构
@@ -36,11 +40,15 @@ inline void CacheManager::destory(){
 }
 inline CacheManager::CacheManager(){
     ::atexit(&CacheManager::destory); // 自动销毁
+    ::atexit(&CacheManager::writeToFile); // 缓存持久化
 }
 
 // 获取Cache
-inline LRUCache& CacheManager::getCache(size_t idx){
-    return getInstance()->_caches[idx];
+inline LRUCache& CacheManager::getKeyWordCache(size_t idx){
+    return getInstance()->_cachesKeyWord[idx];
+}
+inline LRUCache& CacheManager::getWebQueryCache(size_t idx){
+    return getInstance()->_cachesWebQuery[idx];
 }
 
 #endif //_CACHEMANAGER_H
